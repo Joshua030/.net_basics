9a0dc7fd022fb088c309958e9e5ae4fef5ee767f
Joshua030 <Joshua030@users.noreply.github.com>
2026-07-13 23:35:14 +0200

    crud on .net

--- asp_net_course_2026/WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ asp_net_course_2026/WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebDiaryAPI.Data;
 using WebDiaryAPI.Models;
 
@@ -11,11 +12,87 @@ namespace WebDiaryAPI.Controllers
     {
 
         [HttpGet]
-        public IEnumerable<DiaryEntry> GetDiaryEntries()
+        public async Task<ActionResult<IEnumerable<DiaryEntry>>> GetDiaryEntries()
         {
             //return context.DiaryEntries.ToList();
-            return [.. context.DiaryEntries];
+            return await context.DiaryEntries.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DiaryEntry>> GetDiaryEntryById(int id)
+        {
+
+            var diaryEntry = await context.DiaryEntries.FindAsync(id);
+
+            if (diaryEntry == null) return NotFound();
+
+            return diaryEntry;
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<DiaryEntry>> CreateDiaryEntry(DiaryEntry diaryEntry)
+        {
+            diaryEntry.Id = 0;
+
+            context.DiaryEntries.Add(diaryEntry);
+            await context.SaveChangesAsync();
+
+            var resourceUrl = Url.Action(nameof(GetDiaryEntryById), new { id = diaryEntry.Id });
+            return Created(resourceUrl, diaryEntry);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdateDiaryEntryById(int id, [FromBody] DiaryEntry diaryEntry)
+        {
+            if (id != diaryEntry.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(diaryEntry).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+
+                if (!DiaryEntryExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiaryEntryById(int id)
+        {
+            var diaryEntry = await context.DiaryEntries.FindAsync(id);
+            if (diaryEntry == null) return NotFound();
+
+            context.DiaryEntries.Remove(diaryEntry);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+
+        private bool DiaryEntryExist(int id)
+        {
+            return context.DiaryEntries.Any(d => d.Id == id);
+        }
+
+
+
 
     }
 }
